23a0235c5909a57ac690ec1792289225adc543b8
Shresth Rajput <rajshresth2@gmail.com>
2024-07-12 10:02:02 +0530

    Lecture on Operators

---
+++ Javascript/Day-2 Keywords/script.js
@@ -0,0 +1,46 @@
+// let a=5;
+// a+=5
+// console.log(a)
+
+// let a=10;
+// a-=5
+// console.log(a)
+
+
+// let a=2
+// a*=2
+// a=a*2
+// console.log(a)
+
+
+// let a=10
+// a/=2
+// // a=a/2
+// console.log(a)
+
+// let a=2
+// a**=2
+// // a=a**2
+// console.log(a)
+
+
+let name=prompt('Enter name')
+
+if(name=="puneet"){
+    console.log("Ab tu dekh...")
+}
+else if(name=="carry"){
+    console.log("Asambhav...")
+}
+else if(name=="harsh"){
+    console.log("Samajh rhe ho..")
+}
+else if(name=="ashish"){
+    console.log("Bijli ka bill tera baap bharega..")
+}
+else if(name=="elvish"){
+    console.log("Sysssstummmm......")
+}
+else{
+    console.log("Murkh enterr correct name")
+}
