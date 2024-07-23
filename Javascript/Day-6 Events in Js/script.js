ca8bfff22dd4b4edf31e2158f2e95846c1dc5e5b
Shresth Rajput <rajshresth2@gmail.com>
2024-07-23 09:55:51 +0530

    Lecture on Events

---
+++ Javascript/Day-6 Events in Js/script.js
@@ -0,0 +1,8 @@
+const heading1=document.querySelector('h1')
+const heading2=document.querySelector('h2')
+const button=document.getElementById('btn')
+
+button.addEventListener("click",function(){
+    heading1.textContent="Chakde India";
+    heading2.textContent="Me Hu Giyan me hu bada Takatvar mera gala h bohot surila"
+})
